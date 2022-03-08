a74374efd0e8df80da20e55302ab0ac9ff604b07
allanmarmo <allanmarmo@gmail.com>
2022-03-08 13:49:52 -0300

    Seção 11: Beans e Configurations, Swagger

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,11 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+	implementation("io.springfox:springfox-swagger2:3.0.0")
+	implementation("io.springfox:springfox-swagger-ui:3.0.0")
+	implementation("io.springfox:springfox-boot-starter:3.0.0")
+
 	runtimeOnly("mysql:mysql-connector-java")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
